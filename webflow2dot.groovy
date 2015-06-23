9dde88f92f4e5d497daa968284d6da8590411cae
Ed Cole <edcole@mail.sunyjcc.edu>
2015-06-23 17:21:51 -0400

    webflow2dot.groovy - Convert Spring webflow to DOT graph.

---
+++ webflow2dot.groovy
@@ -0,0 +1,108 @@
+/* Convert a Spring webflow XML file to DOT */
+// Get the input and output files.
+def cli = new CliBuilder(usage: "Convert a Spring webflow XML file to DOT");
+cli.h(longOpt: 'help', required: false, 'show usage information' )
+cli.i(longOpt: 'input', required: true, args: 1, 'Input file' )
+cli.o(longOpt: 'output', required: true, args: 1, 'Output file' )
+
+def opt = cli.parse(args);
+if (!opt) {
+  return
+} 
+if (opt.h) {
+  cli.usage()
+  return
+}
+def input = new File(opt.i)
+def output = new File(opt.o)
+
+if (!input.exists()) {
+  System.err.println("Cannot find $input")
+  return;
+}
+
+/** How to portray the states */
+def boxAttrs = [
+  'action-state': [shape: "box"],
+  'decision-state': [shape: "diamond"],
+  'end-state': [shape: "box"],
+  'global-transitions': [shape: "box"],
+  'on-start': [shape: "box"],
+  'subflow-state': [shape: "box"],
+  'view-state': [shape: "box"],
+]
+
+def showStateBox = {
+  state, w ->
+    def boxId = "${state.@id}"
+    if (boxId.size()) {
+      def boxName = boxId?:state.name()
+      def boxRules = boxAttrs[state.name()] + [label: boxName]
+      def attrString = boxRules.collect {
+        key, value ->
+          "$key=$value"
+      }.join(',');
+      w.println "  ${boxName} [$attrString]"
+      state.transition.each {
+        t ->
+          w.println "  ${boxId} -> ${t.@to} [label=\"${t.on}\"]"
+      }
+    }
+}
+
+def elemRules = boxAttrs.keySet().inject([:]) {
+  ruleMap, stateName ->
+    ruleMap[stateName] = showStateBox;
+    return ruleMap;
+}
+
+def extendElem = {
+  String name, Closure c ->
+    def x = elemRules[name]
+    if (x) {
+      elemRules[name] = {
+        state, w ->
+          x(state,w);
+          c(state,w);
+      }
+    } else {
+      elemRules[name] = c;
+    }
+}
+
+extendElem('decision-state') {
+  state, w ->
+    def id = state.@id as String
+    state.if.each {
+        f ->
+          def thenId = f.@then as String
+          def elseId = f.@else as String
+          if (thenId?.size()) {
+            w.println "  $id -> $thenId [label=\"then\"]"
+          }
+          if (elseId?.size()) {
+            w.println "  $id -> $elseId [label=\"else\"]"
+          }
+      }
+}
+
+// Process the input file
+output.withWriter {
+  o ->
+    o.println "digraph webflow {"
+    o.println "  rkdir=LR;"
+    def flow = new XmlSlurper().parse(input)
+    flow.children().each {
+      elem ->
+        def name = elem.name()
+        def rule = elemRules[name] 
+        if (rule) {
+          //def x = elemRules[name]
+          // println ">> $name -> ${x.getClass()}"
+          rule(elem, o)
+        } else {
+          println ">${name}"
+        }
+    }
+    o.println "}"
+}
