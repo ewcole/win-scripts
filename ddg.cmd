3dd1a7b77d1fa41c72a39f7a58289989aefa9e47
Ed Cole <edcole@mail.sunyjcc.edu>
2017-08-28 16:10:48 -0400

    Correct google.cmd and add alternate for Duck Duck Go.

---
+++ ddg.cmd
@@ -0,0 +1,3 @@
+:: Send a query to Duck Duck Go
+@echo off
+start "" "https://duckduckgo.com/?q=%*"
--- google.cmd
+++ google.cmd
@@ -1,2 +1,2 @@
 @echo off
-start http://google.com/search?q=%*
+start "" "http://google.com/search?q=%*"
