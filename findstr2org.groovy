2fa17f36a23b53e0e850683ae95c956358e57fb7
Ed Cole <edcole@mail.sunyjcc.edu>
2020-04-13 09:15:52 -0400

    findstr2org.groovy - Open org files with emacs, add CSS

--- findstr2org.groovy
+++ findstr2org.groovy
@@ -41,7 +41,7 @@ String getProtocol(String fileName) {
   // println "# ext = $ext"
   def extList = ['html': 'file+emacs',
                  'pdf':  'file+sys',
-                 'org': 'file+emacs',
+                 'org': 'file',
                  //'properties': 'file+emacs',
                  'service': 'file+emacs',]
   String protocol = extList.containsKey(ext)?extList[ext]:'file'
@@ -69,10 +69,10 @@ def printDir(ConfigObject cfg, int level, String dirName, String path) {
   if (cfg.textLines.size() || cfg.dir.keySet().size() > 1) {
     println "${'*' * level} $dirName"
     cfg.textLines.each {
-      def textRe = it.replaceAll(/(?i)[^a-z0-9 ]/, '.')
+      def search = "::/${it.replaceAll(/(?i)[^a-z0-9 ]/, '.')}/"
       //println "path=$path"
       String filePath = "$path".replaceAll(/[\/\\]$/, '');
-      println "- [[${getProtocol(dirName)}:$filePath::/$textRe/][Edit]]${getEditLink('',filePath)}: $it"
+      println "- [[${getProtocol(dirName)}:$filePath$search][Edit]]${getEditLink('',filePath)}: $it"
     }
     debug "# cfg.dir=${cfg.dir.keySet()}"
     cfg.dir.keySet().each {
@@ -97,23 +97,89 @@ def printDir(ConfigObject cfg, int level, String dirName, String path) {
 
 def printHeader = {
 println """#+TITLE: Working Files
-#+OPTIONS: ':nil *:t -:t ::t <:t H:3 \\n:nil ^:{} arch:headline
+#+OPTIONS: ':nil *:t -:t ::t <:t H:3 \\n:nil ^:{} arch:headline num:nil
 #+OPTIONS: author:t c:nil creator:comment d:(not "LOGBOOK") date:t
 #+LANGUAGE: en
 #+SELECT_TAGS: export
 #+OPTIONS: html-link-use-abs-url:nil html-postamble:nil
 #+OPTIONS: html-preamble:nil html-scripts:t html-style:t
 #+OPTIONS: html5-fancy:nil tex:t
-#+CREATOR: <a href=\"http://www.gnu.org/software/emacs/\">Emacs</a> 24.2.1 (<a href=\"http://orgmode.org\">Org</a> mode 8.2.6)
-#+HTML_CONTAINER: div
-#+HTML_DOCTYPE: xhtml-strict
-#+HTML_HEAD:
-#+HTML_HEAD_EXTRA:
-#+HTML_LINK_HOME:
-#+HTML_LINK_UP:
-#+HTML_MATHJAX:
-#+INFOJS_OPT:
-#+LATEX_HEADER:""".replaceAll("\r?\n", System.getProperty('line.separator'))
+#+html_head: <style type="text/css">
+#+html_head: body {
+#+html_head:     font-family: Calibri;
+#+html_head: }
+#+html_head: td {
+#+html_head: 	border: 1px solid silver;
+#+html_head: 	padding: 1px 5px;
+#+html_head: }
+#+html_head: 
+#+html_head: th {
+#+html_head: 	color: white;
+#+html_head: 	background-color: #040;
+#+html_head: 	padding: 3px 9px;
+#+html_head:         border:  1px solid white;
+#+html_head: }
+#+html_head: 
+#+html_head: pre {
+#+html_head:    margin: 0;
+#+html_head:    margin-left: 2em;
+#+html_head:    padding: 0 0 0 3px;
+#+html_head: }
+#+html_head: </style>
+#+html_head: <style type="text/css">
+#+html_head: body {
+#+html_head:     font-family: Calibri;
+#+html_head: }
+#+html_head: td {
+#+html_head: 	border: 1px solid silver;
+#+html_head: 	padding: 1px 5px;
+#+html_head: }
+#+html_head: 
+#+html_head: th {
+#+html_head: 	color: white;
+#+html_head: 	background-color: #040;
+#+html_head: 	padding: 3px 9px;
+#+html_head:         border:  1px solid white;
+#+html_head: }
+#+html_head: 
+#+html_head: pre {
+#+html_head:    margin: 0;
+#+html_head:    margin-left: 2em;
+#+html_head:    padding: 0 0 0 3px;
+#+html_head: }
+#+html_head: </style>
+#+html_head: <style type="text/css">
+#+html_head: body {
+#+html_head:     font-family: Calibri;
+#+html_head: }
+#+html_head: td {
+#+html_head: 	border: 1px solid silver;
+#+html_head: 	padding: 1px 5px;
+#+html_head: }
+#+html_head: 
+#+html_head: th {
+#+html_head: 	color: white;
+#+html_head: 	background-color: #040;
+#+html_head: 	padding: 3px 9px;
+#+html_head:         border:  1px solid white;
+#+html_head: }
+#+html_head: 
+#+html_head: pre {
+#+html_head:    margin: 0;
+#+html_head:    margin-left: 2em;
+#+html_head:    padding: 0 0 0 3px;
+#+html_head: }
+#+html_head: ul.org-ul {
+#+html_head: 	list-style: none;
+#+html_head: }
+#+html_head: li {
+#+html_head: 	font-family: Consolas;
+#+html_head: 	background-color: #d6fff9;
+#+html_head: 	border: 1px solid silver;
+#+html_head: 	margin: 3px;
+#+html_head: }
+#+html_head: </style>
+""".replaceAll("\r?\n", System.getProperty('line.separator'))
 }
 printHeader()
 printDir(allFiles, 1, "Files", "");
