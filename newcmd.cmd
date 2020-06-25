edf873e402a923ea6bf7bdfc8402e389eaa2d008
Ed Cole <EdwardWCole@gmail.com>
2020-06-25 08:20:58 -0400

    newcmd - Open a new cmd with the directory name as title

---
+++ newcmd.cmd
@@ -0,0 +1,6 @@
+:: Start a new cmd window with the given title.  This is significant
+::    because Windows keeps the display settings by the window title.
+@echo off
+for /d %%d in (.) do (
+    start "%%~nd" %*
+)
