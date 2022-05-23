b56becc20c018179267c933f7d09333c90d5979c
Ed Cole <EdwardWCole@gmail.com>
2022-05-23 16:41:05 -0400

    Use runemacs instead of emacs in editthese.cmd

--- editthese.cmd
+++ editthese.cmd
@@ -1,4 +1,4 @@
 @echo off
 :: This interprets each line of standard input as the name of a file and opens it in emacs.
 ::   Use with care.
-cat | sed -e "s/^/emacs /;" | cmd
+cat | sed -e "s/^/runemacs /;" | cmd
