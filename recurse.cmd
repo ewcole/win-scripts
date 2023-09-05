ca08ef3632a1cbb4631e6abc82b4bb06d6065499
Ed Cole <EdwardWCole@gmail.com>
2023-09-05 17:47:30 -0400

    Update recurse.cmd

--- recurse.cmd
+++ recurse.cmd
@@ -1,6 +1,15 @@
 @echo off
+rem Usage:  recurse <command>
+rem 
+rem Walk the submodules recursively and execute the command at
+rem every level.  By default, it steps into git submodules, but
+rem if there is a recurse script in a subdirectory, it will call
+rem that one instead.  
+
 setlocal
 set command=%*
+rem Get the subdirectories from .gitmodules.  If it doesn't
+rem exit, don't process any subdirectories
 if exist .gitmodules (
     for /f %%f in ('awk "/path += +/{print $3}" ^< .gitmodules') do (
         if not "%%f" == "echo" (
@@ -8,6 +17,7 @@ if exist .gitmodules (
         )
     )
 )
+rem After the subdirectories are done, process the current directory.
 for %%f in (.) do (
    echo # %%~ff
    %command%
@@ -15,17 +25,18 @@ for %%f in (.) do (
 goto :eof
 
 :processDir
-    :: echo Processing %1
+    rem Replace slashes with backslashes in the directory name
     for /f %%d in ('echo %1 ^| sed s/\//\\/g;') do (
         set dirName=%%d
     )
-    :: echo dirName=%dirName%
     if exist %dirName% (
-        pushd %dirName%
-        :: echo After push to %dirName%
-        for %%c in (.) do (echo # %%~fc)
-        :: echo after echo cd
-        recurse %command%
+        pushd .\%dirName%
+        rem Now that we are in the subdirectory, call
+        rem    recurse again.  We use cmd /c because it has
+        rem    to be in a separate process.  Also, if there
+        rem    is a recurse script in the subdirectory, it
+        rem    will use that one instead.
+        cmd /c recurse %command%
         popd
     )
     goto :eof
