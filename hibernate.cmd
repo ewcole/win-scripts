4be723f7184b205c5dd880bb64e505709e5c3996
Ed Cole <EdwardWCole@gmail.com>
2025-06-12 09:24:58 -0400

    hibernate.cmd

---
+++ hibernate.cmd
@@ -0,0 +1,12 @@
+rem 
+rem Shut down the computer and enable it to restart in the same
+rem state.
+rem 
+rem I don't know the difference between /h and /hybrid
+rem
+shutdown /s /hybrid /t 0
+
+rem https://fossbytes.com/whats-the-difference-between-fast-startup-hybrid-sleep-hibernate-sleep-shutdown/
+rem https://onlinecmag.com/windows-hybrid-shutdown-technique/
+rem https://petri.com/how-to-disable-windows-10-fast-startup-and-why-you-might-want-to/
+rem https://answers.microsoft.com/en-us/windows/forum/all/difference-between-hybrid-shutdown-and-normal/af888b6c-ac6d-4408-a659-9259bd1e9abe
